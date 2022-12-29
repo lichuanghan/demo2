eaba16ee77fa36280c21ca9d007cffd314395f9f
lichunghan <2608367047@qq.com>
2022-12-29 17:23:58 +0800

    Initial commit

---
+++ src/main/java/com/example/demo2/VarDemo.java
@@ -0,0 +1,21 @@
+package com.example.demo2;
+
+public class VarDemo {
+    byte a;
+    short b;
+    int c;
+    char d;
+    float e;
+    double f;
+    boolean g;
+    long h;
+    String i;
+    public void test(){
+        String a = null;
+        System.out.println(b);
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        new VarDemo();
+    }
+}
